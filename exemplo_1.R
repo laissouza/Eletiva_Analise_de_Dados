890bc83ea0f2f375cb4d041b418334a058daa54a
Lais Souza <lais.oliveirasouza@yahoo.com.br>
2021-04-05 11:19:55 -0300

    código comentado com: sumário, plots, estrutura da base de dados, criação de objeto simples e complexo

--- exemplo_1.R
+++ exemplo_1.R
@@ -1,1 +1,19 @@
+# sumário base de dados iris
 summary(iris)
+
+# plots base de dado iris
+plot(iris)
+
+# estrutura base de dados iris
+str(iris)
+
+# criação de objeto simples por vetor
+objeto <- c(24, 3, 7, 59, 92)
+# estrutura do objeto simples
+str(objeto)
+
+# criação de objeto complexo com base de dados iris
+# sintaxe: função(variável_dependente ~ todas_as_variáveis, base_de_dados)
+flores <- lm(Sepal.Length ~ ., iris)
+# estrutura do objeto complexo
+str(flores)
